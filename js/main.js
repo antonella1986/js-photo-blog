6cdc0847df0131dcc64793e73587fbe458a610a3
antonella1986 <antonella.leo.mail@gmail.com>
2025-02-28 17:04:19 +0100

    add cards' padding

--- js/main.js
+++ js/main.js
@@ -1,31 +1,35 @@
-// dichiaro la costante (immagini)
-
-const imgEl = document.querySelector(".imgs")
-
 function getImages() {
     // fetcho
     fetch("https://lanciweb.github.io/demo/api/pictures/")
-    .then(response => url.json())
-    .then(data => {
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        
         // ciclo for per generare le immagini
-        for (let i = 0; i < 6; i++) {
+        const thoseCards = document.querySelector(".cards")
+        for (let i = 0; i < data.length; i++) {
+            const thisCard = data[i]
+            console.log(thisCard);
+            
+            // preparo il markup
             const markup = `
-            <div class="col-4">
+            <div class="col-4 mb-3">
                 <div class="card" style="width: 16rem;">
                     <img class="pin" src="./assets/img/pin.svg" alt="">
                     <div class="img">
-                        <img class="imgs" src=`${imageEl}` alt="">
+                        <img class="imgs" src="${thisCard.url}" alt="">
                     </div>
-                    <p class="card-text text-body-tertiary pt-3 m-0">01-07-2024</p>
-                    <h5 class="titoli"><strong>ALBEROBELLO</strong></h5>
+                    <p class="card-text text-body-tertiary pt-3 m-0">${thisCard.date}</p>
+                    <h5 class="titoli"><strong>${thisCard.title}</strong></h5>
                 </div>
             </div>
             `
-            imgEl.insertAdjacentHTML("beforeend", markup)
+            // inserisco il nuovo HTML nella DOM
+            thoseCards.innerHTML += markup
         }
     })
-    .catch(error => {
-        console.log("Non sono riuscito a generare le immagini", error); 
+    .catch((error) => {
+        console.error(error.message); 
     })
 }
 
